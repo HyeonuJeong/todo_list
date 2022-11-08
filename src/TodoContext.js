932050abd08e60f0fd039117b18e20b6ec5f94e5
HyeonuJeong <hyeonu4945@gmail.com>
2022-11-08 19:12:58 +0900

    todolist

---
+++ src/TodoContext.js
@@ -0,0 +1,70 @@
+import React, { useReducer, createContext, useContext, useRef } from 'react';
+
+const initialTodos = [
+  {
+    id: 1,
+    text: "리액트 기초 문법",
+    done: false,
+  },
+  {
+    id: 2,
+    text: "컴포넌트 구조 이해",
+    done: false,
+  },
+  {
+    id: 3,
+    text: "Hook 활용",
+    done: false,
+  },
+  {
+    id: 4,
+    text: "Todolist 만들기",
+    done: false,
+  },
+];
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return state.concat(action.todo);
+    case 'TOGGLE':
+      return state.map(todo =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    case 'REMOVE':
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+const TodoStateContext = createContext();
+const TodoDispatchContext = createContext();
+const TodoNextIdContext = createContext();
+
+export function TodoProvider({ children }) {
+  const [state, dispatch] = useReducer(todoReducer, initialTodos);
+  const nextId = useRef(5);
+
+  return (
+    <TodoStateContext.Provider value={state}>
+      <TodoDispatchContext.Provider value={dispatch}>
+        <TodoNextIdContext.Provider value={nextId}>
+          {children}
+        </TodoNextIdContext.Provider>
+      </TodoDispatchContext.Provider>
+    </TodoStateContext.Provider>
+  );
+}
+
+export function useTodoState() {
+  return useContext(TodoStateContext);
+}
+
+export function useTodoDispatch() {
+  return useContext(TodoDispatchContext);
+}
+
+export function useTodoNextId() {
+  return useContext(TodoNextIdContext);
+}
